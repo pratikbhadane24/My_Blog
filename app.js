03fe32ec623f8ec7c4b6ac65a36e496cb2326b5c
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-08-19 00:43:31 +0530

    ch7 (added compose page)

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.get("/contact", function (req, res) {
   res.render("contact", { Contact: contactContent })
 });
 
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
 
 
 

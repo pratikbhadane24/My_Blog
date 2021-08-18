aaf47a4996d6b2193924c137c8fb6d9ca1e28d3e
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-08-18 22:39:21 +0530

    ch2

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  res.render("home")
+app.get("/", function (req, res) {
+  res.render("home", { StartingContent: homeStartingContent })
 });
 
 
@@ -28,6 +28,6 @@ app.get("/", function(req, res){
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
